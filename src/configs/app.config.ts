3d088c075238ccfc550ba629aa5719d69ddb4b96
Anucha.tk <anuchataowkaen@gmail.com>
2023-04-08 22:37:15 +0700

    feat(app): fix app main, swagger etc

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,10 +1,24 @@
 import { registerAs } from '@nestjs/config';
-
+import { APP_LANGUAGE } from 'src/app/constants/app.constant';
+import { version } from 'package.json';
+import { ENUM_APP_ENVIRONMENT } from 'src/app/constants/app.enum.constant';
 export default registerAs(
   'app',
   (): Record<string, any> => ({
+    name: process.env.APP_NAME,
+    env: process.env.APP_ENV ?? ENUM_APP_ENVIRONMENT.DEVELOPMENT,
+    language: process.env.APP_LANGUAGE?.split(',') ?? [APP_LANGUAGE],
+    repoVersion: version,
+    versioning: {
+      enable: process.env.HTTP_VERSIONING_ENABLE === 'true' ?? 'false',
+      prefix: 'v',
+      version: process.env.HTTP_VERSION ?? '1',
+    },
+    globalPrefix: '/api',
     http: {
-      port: Number.parseInt(process.env.APP_PORT) || 5000,
+      enable: process.env.HTTP_ENABLE === 'true' ?? false,
+      host: process.env.HTTP_HOST ?? 'localhost',
+      port: Number.parseInt(process.env.HTTP_PORT) || 5000,
     },
   }),
 );
--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -3,9 +3,11 @@ import { registerAs } from '@nestjs/config';
 export default registerAs(
   'database',
   (): Record<string, any> => ({
-    host: process.env.DB_HOST,
-    name: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_USER_PWD,
+    host: process.env?.DATABASE_HOST ?? 'mongodb://localhost:27017',
+    name: process.env?.DATABASE_NAME,
+    user: process.env?.DATABASE_USER,
+    password: process?.env.DATABASE_PASSWORD,
+    debug: process.env.DATABASE_DEBUG === 'true',
+    options: process.env?.DATABASE_OPTIONS,
   }),
 );
