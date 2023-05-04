41e4e5dca5d4ab5599f138ea9faf1349e3070d1e
Anucha.tk <anuchataowkaen@gmail.com>
2023-05-04 13:20:22 +0700

    feat(request): id middleware

--- src/common/request/middleware/request.middleware.module.ts
+++ src/common/request/middleware/request.middleware.module.ts
@@ -1,9 +1,10 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { RequestHelmetMiddleware } from './helmet/request.helmet.middleware';
+import { RequestIdMiddleware } from './id/request.id.middleware';
 
 @Module({})
 export class RequestMiddlewareModule implements NestModule {
   configure(consumer: MiddlewareConsumer): void {
-    consumer.apply(RequestHelmetMiddleware).forRoutes('*');
+    consumer.apply(RequestHelmetMiddleware, RequestIdMiddleware).forRoutes('*');
   }
 }
